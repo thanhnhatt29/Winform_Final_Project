9f4b8ec2f3b8ebe3886192149e1b97f076dccc9f
Thang <104261834+sasageyo0169@users.noreply.github.com>
2022-05-19 18:03:42 +0700

    TH

--- Final_Form/BookAdd.Designer.cs
+++ Final_Form/BookAdd.Designer.cs
@@ -34,6 +34,8 @@
             this.label3 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // AddBook
@@ -42,7 +44,7 @@
             this.AddBook.Name = "AddBook";
             this.AddBook.Size = new System.Drawing.Size(148, 23);
             this.AddBook.TabIndex = 0;
-            this.AddBook.Text = "Them Sach";
+            this.AddBook.Text = "Thêm Sách";
             this.AddBook.UseVisualStyleBackColor = true;
             this.AddBook.Click += new System.EventHandler(this.AddBook_Click);
             // 
@@ -58,9 +60,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(106, 88);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 16);
+            this.label1.Size = new System.Drawing.Size(63, 16);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Ten Sach";
+            this.label1.Text = "Tên sách";
             // 
             // label3
             // 
@@ -69,7 +71,7 @@
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(56, 16);
             this.label3.TabIndex = 5;
-            this.label3.Text = "The loai";
+            this.label3.Text = "Thể loại";
             // 
             // comboBox1
             // 
@@ -81,7 +83,8 @@
             // 
             // label2
             // 
-            this.label2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
             this.label2.Location = new System.Drawing.Point(189, 20);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(227, 47);
@@ -89,11 +92,22 @@
             this.label2.Text = "Thêm sách";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Final.Properties.Resources._718323;
+            this.pictureBox1.Location = new System.Drawing.Point(186, 160);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(33, 23);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
             // BookAdd
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(569, 213);
+            this.ClientSize = new System.Drawing.Size(569, 251);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label3);
@@ -102,6 +116,7 @@
             this.Controls.Add(this.AddBook);
             this.Name = "BookAdd";
             this.Text = "BookAdd";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -115,5 +130,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
